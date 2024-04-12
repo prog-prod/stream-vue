bf7cb6e9455970f7d2cde4dad5789ad28358ecc7
Andrii Polyvianyi <polyvyanyy.andrii@gmail.com>
2024-04-12 12:39:04 +0300

    added compatibility with vue 3

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-import type Vue from 'vue'
-
-const extendVue: (typeof Vue)['extend'] = (options: any) => options
+import { defineComponent, ref } from 'vue'
 
 const scriptLocation =
   'https://embed.videodelivery.net/embed/r4xu.fla9.latest.js'
 
-export type HTMLStreamElement = HTMLVideoElement
+export type HTMLStreamElement = HTMLScriptElement
 
 declare global {
   interface Window {
@@ -16,14 +14,6 @@ declare global {
   }
 }
 
-// eslint-disable-next-line
-const _Vue = require('vue')
-if (_Vue.config) {
-  _Vue.config.ignoredElements = [
-    ...(_Vue.config.ignoredElements || []),
-    'stream',
-  ]
-}
 
 export type Events =
   /**
@@ -123,183 +113,80 @@ export type Events =
    */
   | 'stream-adtimeout'
 
-export const VideoStream = extendVue({
+export const VideoStream = defineComponent({
   name: 'VideoStream',
   props: {
-    /**
-     * Tells the browser to immediately start downloading the video and play it as soon as it can. Note that mobile browsers generally do not support this attribute, the user must tap the screen to begin video playback. Please consider mobile users or users with Internet usage limits as some users don’t have unlimited Internet access before using this attribute.
-     *
-     * To disable video autoplay, the autoplay attribute needs to be removed altogether as this attribute. Setting autoplay="false" will not work; the video will autoplay if the attribute is there in the <stream> tag.
-     *
-     * In addition, some browsers now prevent videos with audio from playing automatically. You may add the mute attribute to allow your videos to autoplay. For more information, [go here](https://webkit.org/blog/6784/new-video-policies-for-ios/).
-     *
-     * @default false
-     */
     autoplay: { type: Boolean, default: false },
-    /**
-     * Shows the default video controls such as buttons for play/pause, volume controls. You may choose to build buttons and controls that work with the player.
-     *
-     * @default false
-     */
     controls: { type: Boolean, default: false },
-    /**
-     * Returns the current playback time in seconds. Setting this value seeks the video to a new time.
-     *
-     * @default 0
-     */
     currentTime: { type: Number, default: 0 },
-    /**
-     * The height of the video’s display area, in CSS pixels.
-     */
     height: { type: [String, Number] },
-    /**
-     * The width of the video’s display area, in CSS pixels.
-     */
     width: { type: [String, Number] },
-    /**
-     * A Boolean attribute which indicates the default setting of the audio contained in the video. If set, the audio will be initially silenced.
-     *
-     * @default false
-     */
     muted: { type: Boolean, default: false },
-    /**
-     * A Boolean attribute; if included the player will automatically seek back to the start upon reaching the end of the video.
-     *
-     * @default false
-     */
     loop: { type: Boolean, default: false },
-    /**
-     * This enumerated attribute is intended to provide a hint to the browser about what the author thinks will lead to the best user experience. You may choose to include this attribute as a boolean attribute without a value, or you may specify the value preload="auto" to preload the beginning of the video. Not including the attribute or using preload="metadata" will just load the metadata needed to start video playback when requested.
-     *
-     * The <video> element does not force the browser to follow the value of this attribute; it is a mere hint. Even though the preload="none" option is a valid HTML5 attribute, Stream player will always load some metadata to initialize the player. The amount of data loaded in this case is negligable.
-     */
     preload: {
       type: [String, Boolean] as unknown as () =>
-        | 'auto'
-        | 'metadata'
-        | 'none'
-        | boolean,
+          | 'auto'
+          | 'metadata'
+          | 'none'
+          | boolean,
     },
-    /**
-     * Sets volume from 0.0 (silent) to 1.0 (maximum value)
-     *
-     * @default 1
-     */
     volume: { type: Number, default: 1 },
   },
-  inheritAttrs: false,
-  data: () => ({
-    streamScript: null as HTMLScriptElement | null,
-  }),
-  mounted() {
-    ;(this as any).initialiseStream()
-  },
-  watch: [
-    'autoplay',
-    'controls',
-    'currentTime',
-    'muted',
-    'loop',
-    'volume',
-    'preload',
-  ].reduce((obj, prop) => {
-    obj[prop] = function (
-      this: { updateProp: (key: string, value: any) => void },
-      val: any
-    ) {
-      this.updateProp(prop, val)
+  setup(props, { emit }) {
+    const streamScript = ref<HTMLScriptElement | null>(null)
+
+    const initialiseStream = () => {
+      if (!streamScript.value) {
+        const script = document.createElement('script')
+        script.setAttribute('data-cfasync', 'false')
+        script.setAttribute('defer', 'true')
+        script.setAttribute('type', 'text/javascript')
+        script.setAttribute('src', scriptLocation)
+        document.head.appendChild(script)
+
+        streamScript.value = script
+      }
+
+      if (window.__stream && streamScript.value) {
+        window.__stream.initElement(streamScript.value as HTMLStreamElement)
+      }
     }
-    return obj
-  }, {} as Record<string, any>),
+
+    return {
+      initialiseStream,
+      streamScript,
+    }
+  },
+  mounted() {
+    this.initialiseStream()
+  },
+  watch: {
+    autoplay: function (val: any) {
+      this.updateProp('autoplay', val)
+    },
+    controls: function (val: any) {
+      this.updateProp('controls', val)
+    },
+    currentTime: function (val: any) {
+      this.updateProp('currentTime', val)
+    },
+    muted: function (val: any) {
+      this.updateProp('muted', val)
+    },
+    loop: function (val: any) {
+      this.updateProp('loop', val)
+    },
+    volume: function (val: any) {
+      this.updateProp('volume', val)
+    },
+    preload: function (val: any) {
+      this.updateProp('preload', val)
+    },
+  },
   methods: {
     updateProp(key: string, value: any) {
       if (!this.$refs.stream) return
-      ;(this.$refs.stream as any)[key] = value
-    },
-    initialiseStream() {
-      this.streamScript =
-        this.streamScript ||
-        document.querySelector<HTMLScriptElement>(
-          `script[src="${scriptLocation}"]`
-        )
-
-      if (!this.streamScript) {
-        const streamScript = document.createElement('script')
-        streamScript.setAttribute('data-cfasync', 'false')
-        streamScript.setAttribute('defer', 'true')
-        streamScript.setAttribute('type', 'text/javascript')
-        streamScript.setAttribute('src', scriptLocation)
-        document.head.appendChild(streamScript)
-
-        this.streamScript = streamScript
-      }
-
-      if (window.__stream && this.$refs.stream) {
-        window.__stream.initElement(this.$refs.stream as HTMLStreamElement)
-      }
-    },
-  },
-  render(h) {
-    return h(
-      'div',
-      {
-        style: {
-          height: this.height ? this.height + 'px' : undefined,
-          width: this.width ? this.width + 'px' : undefined,
-        },
-        class: this.$attrs.class,
-      },
-      [
-        h('stream', {
-          attrs: {
-            ...this.$attrs,
-            class: null,
-            style: null,
-            autoplay: this.autoplay,
-            controls: this.controls,
-            currentTime: this.currentTime,
-            muted: this.muted,
-            loop: this.loop,
-            volume: this.volume,
-            preload:
-              typeof this.$attrs.preload === 'string'
-                ? // if it's a string pass as is
-                  this.$attrs.preload
-                : // else if it's true, map to auto
-                this.$attrs.preload === true
-                ? 'auto'
-                : // otherwise (undefined | false) maps to none
-                  'none',
-          },
-          on: this.$listeners,
-          ref: 'stream',
-          tag: 'stream',
-        }),
-      ]
-    )
-  },
-  ...{
-    head: {
-      script: [
-        {
-          src: scriptLocation,
-          'data-cfasync': false,
-          defer: true,
-          type: 'text/javascript',
-          hid: 'cloudflare-stream-script',
-        },
-      ],
-    },
-    metaInfo: {
-      script: [
-        {
-          src: scriptLocation,
-          'data-cfasync': false,
-          defer: true,
-          type: 'text/javascript',
-          vmid: 'cloudflare-stream-script',
-        },
-      ],
+          ;(this.$refs.stream as any)[key] = value
     },
   },
 })
